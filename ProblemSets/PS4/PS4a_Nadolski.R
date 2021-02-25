33c91d64e1dfdb3a81876b238bcdec87baa1d154
Karley Nadolski <karleynadolski@Karleys-MacBook-Pro.local>
2021-02-25 16:25:34 -0600

    Updating PS4a_Nadolski.R

--- ProblemSets/PS4/PS4a_Nadolski.R
+++ ProblemSets/PS4/PS4a_Nadolski.R
@@ -1,10 +1,11 @@
 library(jsonlite)
 library(tidyverse)
 
-system('wget -O dates.json https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20210219&lang=en' )
-system(cat dates.json)
+system('wget -O dates.json "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20210219&lang=en"' )
+system("cat dates.json")
 
 mylist <- fromJSON('dates.json')
 mydf <- bind_rows(mylist$result[-1])
 class(mydf)
+class(mydf$date)
 head(mydf)
