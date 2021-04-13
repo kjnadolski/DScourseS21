2267d47951cc1a10935ed300768d44c5b058e08f
Karley Nadolski <karleynadolski@Karleys-MacBook-Pro.local>
2021-04-13 13:11:38 -0500

    Updating PS9 R code

--- ProblemSets/PS9/PS9_Nadolski.R
+++ ProblemSets/PS9/PS9_Nadolski.R
@@ -18,7 +18,7 @@ library(magrittr)
       housing_test  <- testing(housing_split)
       
   # Create a recipe (takes the log of housing value, converts chas to a factor, creates 6th degree polynomials of each of the continuous features)
-      housing_recipe <- recipe(medv ~ ., data = housing) %>%
+      housing_recipe <- recipe(medv ~ ., data = housing_train) %>%
         # convert outcome variable to logs
         step_log(all_outcomes()) %>%
         # convert 0/1 chas to a factor
@@ -26,31 +26,27 @@ library(magrittr)
         # create interaction term between crime and nox
         step_interact(terms = ~ crim:zn:indus:rm:age:rad:tax:ptratio:b:lstat:dis:nox) %>%
         # create square terms of some continuous variables
-        step_poly(crim,zn,indus,rm,age,rad,tax,ptratio,b,lstat,dis,nox, degree=6)
+        step_poly(crim,zn,indus,rm,age,rad,tax,ptratio,b,lstat,dis,nox, degree=6) %>%
+        # prep it
+        prep()
       
       # Run the recipe
-      housing_prep          <- housing_recipe %>% prep(housing_train, retain = TRUE)
-      housing_train_prepped <- housing_prep %>% juice
-      housing_test_prepped  <- housing_prep %>% bake(new_data = housing_test)
-      
-      # create x and y training and test data
-      housing_train_x <- housing_train_prepped %>% select(-medv)
-      housing_test_x  <- housing_test_prepped  %>% select(-medv)
-      housing_train_y <- housing_train_prepped %>% select( medv)
-      housing_test_y  <- housing_test_prepped  %>% select( medv)
+      housing_train_prepped <- housing_recipe %>% juice
+      housing_test_prepped  <- housing_recipe %>% bake(new_data = housing_test)
     
       # What is the dimension of the training data? 
-          dim(housing_train_x) # The housing_train_x data has dimensions 405 x 74
+          dim(housing_train_prepped) # The housing_train_prepped data has dimensions 405 x 75
       # How many more x variables do you have than in the original data? 
           # There are 60 more x variables after using the recipe function in the testing data (74 compared to 60)
-          
+
+######################################################################################################################
   # Estimate a LASSO model to predict log median housing value
       # penalty parameter lambda is tuned by 6-fold cross validation
           
           # Cross-validate the lambda
           tune_spec <- linear_reg(
             penalty = tune(), # tuning parameter
-            mixture = 1       # 1 = lasso (L1), 0 = ridge (L2)
+            mixture = 1       # 1 = lasso, 0 = ridge
           ) %>% 
             set_engine("glmnet") %>%
             set_mode("regression")
@@ -59,12 +55,12 @@ library(magrittr)
           lambda_grid <- grid_regular(penalty(), levels = 50)
           
           # 6-fold cross-validation
-          rec_folds <- vfold_cv(housing_train_x %>% bind_cols(tibble(medv = housing_train_y$medv)), v = 6)
+          rec_folds <- vfold_cv(housing_train_prepped, v = 6)
           
           # Workflow
           rec_wf <- workflow() %>%
             add_model(tune_spec) %>%
-            add_formula(medv ~ .)
+            add_formula(log(medv) ~ .)
           
           # Tuning results
           rec_res <- rec_wf %>%
@@ -75,25 +71,28 @@ library(magrittr)
           
           top_rmse  <- show_best(rec_res, metric = "rmse")
           best_rmse <- select_best(rec_res, metric = "rmse")
-          
           # Now train with tuned lambda
-          final_lasso <- finalize_workflow(rec_wf, best_rmse)
+          final_lasso <- finalize_workflow(rec_wf,
+                                           best_rmse
+          )
           
-          # Print out results in test set
-          last_fit(final_lasso, split = housing_split) %>%
+          # Print out results
+          last_fit <- last_fit(final_lasso,housing_split) %>%
             collect_metrics() %>% print
+          best_rmse
           
-              # rmse = 4.62
-              # rsq = 0.762
-              # best penalty = 0.00569
+              # rmse = 0.22 (out-of-sample rmse)
+              # rsq = 0.774
+              # best penalty = 0.00356
           
+#######################################################################################################################
     # Estimate a ridge regression model to predict log median housing value
        # penalty parameter lambda is tuned by 6-fold cross validation
           
           # Cross-validate the lambda
           tune_spec <- linear_reg(
             penalty = tune(), # tuning parameter
-            mixture = 0       # 1 = lasso (L1), 0 = ridge (L2)
+            mixture = 0       # 1 = lasso, 0 = ridge
           ) %>% 
             set_engine("glmnet") %>%
             set_mode("regression")
@@ -102,12 +101,12 @@ library(magrittr)
           lambda_grid <- grid_regular(penalty(), levels = 50)
           
           # 6-fold cross-validation
-          rec_folds <- vfold_cv(housing_train_x %>% bind_cols(tibble(medv = housing_train_y$medv)), v = 6)
+          rec_folds <- vfold_cv(housing_train_prepped, v = 6)
           
           # Workflow
           rec_wf <- workflow() %>%
             add_model(tune_spec) %>%
-            add_formula(medv ~ .)
+            add_formula(log(medv) ~ .)
           
           # Tuning results
           rec_res <- rec_wf %>%
@@ -116,20 +115,18 @@ library(magrittr)
               grid = lambda_grid
             )
           
+          # What's the best value of lambda? 
           top_rmse  <- show_best(rec_res, metric = "rmse")
           best_rmse <- select_best(rec_res, metric = "rmse")
+          final_ridge <- finalize_workflow(rec_wf,
+                                           best_rmse
+          )
           
-          # Now train with tuned lambda
-          final_ridge <- finalize_workflow(rec_wf, best_rmse)
-          
-          # Print out results in test set
-          last_fit(final_ridge, split = housing_split) %>%
+          # Print out results
+          last_fit(final_ridge,housing_split) %>%
             collect_metrics() %>% print
+          best_rmse
           
-          # rmse = 4.57
-          # rsq = 0.769
-          # best penalty = 0.00569
-          
-          library(yardstick)
-          
-    
+          # rmse = 0.218
+          # rsq = 0.784
+          # best penalty = 0.0233
